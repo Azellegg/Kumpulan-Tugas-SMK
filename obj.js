e0c45d3950f1d72524472a76fe29951af64d0651
William S <112236831+Azellegg@users.noreply.github.com>
2023-04-13 11:56:02 +0700

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,13 @@
+// TUGAS NOMER 1
+let obj = { nama: "john", umur: 22, bahasa: "indonesia" };
+console.log(`${obj.bahasa}`); 
+
+// TUGAS NOMERR 2
+let arrayDaftarPeserta = ["John Doe", "laki-laki", "baca buku", "1992"];
+let objDaftarPeserta = {
+     nama: arrayDaftarPeserta[0],
+     jenis_kelamin: arrayDaftarPeserta[1],
+     hobi: arrayDaftarPeserta[2],
+     tahun_lahir: arrayDaftarPeserta[3],
+};
+console.log (objDaftarPeserta);
---
+++ questpropertyobject.js
@@ -0,0 +1,33 @@
+// Object Car
+let arrycar = ["Toyota", "2000CC", "200 Kg", "Red", "Dual Turbo Charge", "2 Orang"]
+let objcar = {
+     Merk : arrycar[0],
+     Kapasitas_Mesin : arrycar[1],
+     Berat : arrycar[2],
+     Warna : arrycar[3],
+     Tipe_Mesin : arrycar[4],
+     Kapasitas : arrycar[5],
+};
+console.log(objcar);
+
+// Object Kereta
+let arrykereta = ["KAI", "Disel", "10 Meter", "Putih", "1000 Human"]
+let objkereta = {
+     Merk : arrykereta[0],
+     Bahan_Bakar : arrykereta[1],
+     Panjang : arrykereta[2],
+     Warna : arrykereta[3],
+     Kapasitas : arrykereta[4],
+};
+console.log(objkereta);
+
+// Object Game 
+let arrygame = ["Free Fire", "Mobile Legends", " Apex Legends", "ARK", "Durango Wild Land"]
+let objgame = {
+     Most_Game_1 : arrygame[0],
+     Most_Game_2 : arrygame[1],
+     Most_Game_3 : arrygame[2],
+     Most_Game_4 : arrygame[3],
+     Most_Game_5 : arrygame[4],
+};
+console.log(objgame);
